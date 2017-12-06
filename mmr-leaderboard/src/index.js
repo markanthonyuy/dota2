21addc166d25ccb9ee722e9c1602b7310766cbdf
markanthonyuy <macmac.uy@gmail.com>
2017-12-06 10:28:55 +0800

    remove SW

--- mmr-leaderboard/src/index.js
+++ mmr-leaderboard/src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './css/index.css';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<BrowserRouter basename="/dota2-mmr-leaderboard"><App /></BrowserRouter>, document.getElementById('root'));
-registerServiceWorker();
